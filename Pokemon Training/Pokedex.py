04e033c0062a61f95b8e73a9f53b8faa34ec3446
purejarrett <purejarrett@gmail.com>
2020-07-20 13:56:07 -0400

    Updating the pokemon files

--- Pokemon Training/Pokedex.py
+++ Pokemon Training/Pokedex.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-%matplotlib inline
+# %matplotlib inline
 
 pokemon = pd.read_csv("pokemon.csv")
+
+print(pokemon.head(5))
+
+pokemon2 = pokemon["abilities"].str.split(", ", n = 1, expand = True)
+pokemon2
