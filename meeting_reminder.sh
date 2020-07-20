672195da6c0ce1e4ea9ae370933fb64ad8a6ba7d
purejarrett <61511298+purejarrett@users.noreply.github.com>
2020-07-20 14:56:33 -0400

    added "echo $meeting_info "

--- meeting_reminder.sh
+++ meeting_reminder.sh
@@ -5,6 +5,9 @@ meeting_info=$(zenity - - forms \
     - -add-calendar 'Date' - -add-entry 'Title' \
     - -add-entry 'Emails' \
     2>/dev/null)
+    
+echo $meeting_info    
+    
 if [[ -n "$meeting_info" ]]; then
     python3 send_reminders.py "$meeting_info"
 fi
